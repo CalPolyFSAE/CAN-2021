a642ad9e11f545532c3a18074b325498cc5a3cf5
Seena Abed <66761673+seena18@users.noreply.github.com>
2021-10-23 16:27:48 -0700

    Cleaned up code, adding comments

--- source/CanMessage.h
+++ source/CanMessage.h
@@ -1,21 +1,25 @@
 #ifndef CANMESSAGE_H
 #define CANMESSAGE_H
 
+
+//CanMessage class declaration
 class CanMessage {
 public:
     static void sendTime();
-    static void sendSixtyHz();
-    static void sendHunHz();
+    static void sendSensors();
+    static void sendFourSGauges();
+    static void sendLastSGauge();
 
 private:
     static CanMessage *getInstance();
     void sendTimeHelper();
-    void sendSixtyHzHelper();
-    void sendHunHzHelper();
+    void sendSensorsHelper();
+    void sendFourSGaugesHelper();
+    void sendLastSGaugeHelper();
     CanMessage();
     void initCan();
     static CanMessage* instance;
 };
 
 
-#endif //CANMESSAGE_H
+#endif 
